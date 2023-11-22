cb2c8ac8019bdba615f2f1c7e16d5c4bd93c9c9a
clairemuiru1 <muiruclaire@gmail.com>
2023-11-22 21:59:52 +0300

    "python function"

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print(f"Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("claire")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet_with_default()        
+greet_with_default("Guido") 
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number/2
